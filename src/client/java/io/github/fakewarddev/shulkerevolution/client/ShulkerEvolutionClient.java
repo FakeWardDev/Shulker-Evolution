5122046dee770f2bd591f431fa58460a96cc5284
FakeWardDev <fakewarddev@outlook.fr>
2026-07-04 13:55:59 +0200

    Initial commit

---
+++ src/client/java/io/github/fakewarddev/shulkerevolution/client/ShulkerEvolutionClient.java
@@ -0,0 +1,10 @@
+package io.github.fakewarddev.shulkerevolution.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShulkerEvolutionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
